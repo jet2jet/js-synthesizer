df49fbd5e7af7032e8e64c4f7e5c7044fb5da11a
jet2jet <34238471+jet2jet@users.noreply.github.com>
2018-11-29 21:48:44 +0900

    expose some fields for audio worklet

--- src/main/workletEntry.ts
+++ src/main/workletEntry.ts
@@ -1,4 +1,12 @@
 
 import registerAudioWorkletProcessor from './registerAudioWorkletProcessor';
 
+import { rewriteEventData } from './ISequencerEventData';
+import Synthesizer from './Synthesizer';
+
+AudioWorkletGlobalScope.Fluid = {
+	rewriteEventData: rewriteEventData,
+	Synthesizer: Synthesizer
+};
+
 registerAudioWorkletProcessor();
